9c423535d10d6239569cc8d7fc4b7bd74792855a
nghia nguyen <futuregohan1997@gmail.com>
2020-11-08 15:02:04 +0700

    make it better

--- index.py
+++ index.py
@@ -4,12 +4,41 @@ inputpdf = PdfFileReader(open("Five Short Plays.pdf", "rb"))
 
 
 
-list_page = [0,1,2,3,5]
+# list_page = [0,1,2,3,5]
 
+# output = PdfFileWriter()
+# for i in list_page:
+#     output.addPage(inputpdf.getPage(i))
+
+# with open("reading_day1.pdf", "wb") as outputStream:
+#     output.write(outputStream)
+
+
+
+nb_day = 2
+page_start = 6
+page_end = page_start+3
+
+# list_page = [0,1,2,3,5]
+list_page = list(range(page_start,page_end))
 output = PdfFileWriter()
 for i in list_page:
     output.addPage(inputpdf.getPage(i))
 
-with open("reading_day1.pdf", "wb") as outputStream:
+with open("reading_day%s.pdf"%nb_day, "wb") as outputStream:
     output.write(outputStream)
 
+
+for nb_day in range(3,10):
+    # nb_day = 3
+    page_start = page_end
+    page_end = page_start+3
+
+    # list_page = [0,1,2,3,5]
+    list_page = list(range(page_start,page_end))
+    output = PdfFileWriter()
+    for i in list_page:
+        output.addPage(inputpdf.getPage(i))
+
+    with open("reading_day%s.pdf"%nb_day, "wb") as outputStream:
+        output.write(outputStream)
